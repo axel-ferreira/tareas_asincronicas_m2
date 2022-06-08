c0172c8f68e5ff1458f4443ca8a5ea5ad1de4fa5
axel ferreira <axeljoaopabloferreira@gmail.com>
2022-06-08 18:00:40 -0400

    tareas_asincronicas_m2

---
+++ tarea_1/calculadora.js
@@ -0,0 +1,5 @@
+const edad = prompt ("ingrese su edad");
+edad = parseInt (edad);
+const dias = edad * 365 ;
+alert ("has vivido " + dias + " días.");
+
---
+++ tarea_1/snacks.js
@@ -0,0 +1,10 @@
+
+
+const miedad = 26;
+const restantes= 81 - miedad;
+const snack = ramitas;
+const cuantosalaño = 3 * 52;
+const cuantosmequedan = restantes * cuantosalaño;
+alert ("Necesitaré " + cuantosmequedan " " + snack " para que me alcancen hasta los 81 años." );
+const gasto = 650 * cuantosmequedan;
+alert ("voy a gastar " + gasto + " pesos el resto de mi vida.");
